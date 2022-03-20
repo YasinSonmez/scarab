757562bc9149be9a357d20ea40466bec3e3e053d
kofyou <25074053+kofyou@users.noreply.github.com>
2022-03-20 08:33:44 +0800

    lab5 support no aliasing & stat

--- src/bp/perceptron.cc
+++ src/bp/perceptron.cc
@@ -37,20 +37,57 @@ extern "C" {
 
 #define DEBUG(proc_id, args...) _DEBUG(proc_id, DEBUG_BP_DIR, ##args)
 // (experimental) optimal threshold: a function of the history length
-#define BP_PERCEPTRON_THRESHOLD (int32)(1.93 * (BP_PERCEPTRON_HIST_LENGTH) + 14))
+#define BP_PERCEPTRON_THRESHOLD (int32)(1.93 * BP_PERCEPTRON_HIST_LENGTH + 14)
 
 namespace {
 
 struct Entry {
+  // when aliasing
   std::vector<int> perceptron;
   Addr tag;
 
-  void reset_perceptron() {
-    std::fill(perceptron.begin(), perceptron.end(), 0);
+  // when no aliasing
+  std::map<Addr, std::vector<int> > perceptrons;
+
+  Addr get_tag(const Addr addr) {
+    if (BP_PERCEPTRON_NO_ALIASING == FALSE) {
+      return tag;
+    } else {
+      // https://stackoverflow.com/questions/1409454/c-map-find-to-possibly-insert-how-to-optimize-operations
+      std::map<Addr, std::vector<int> >::iterator it;
+      it = perceptrons.find(addr);
+      if (it == perceptrons.end()){
+        return 0;
+      } else {
+        return addr;
+      }
+    }
   }
 
+  std::vector<int>& get_perceptron(const Addr addr) {
+    if (BP_PERCEPTRON_NO_ALIASING == FALSE) {
+      return perceptron;
+    } else {
+      // https://stackoverflow.com/questions/1409454/c-map-find-to-possibly-insert-how-to-optimize-operations
+      std::map<Addr, std::vector<int> >::iterator it;
+      it = perceptrons.find(addr);
+      if (it == perceptrons.end()){
+        // when do not exist, set all weights to zere
+        // default ctor, and resize
+        perceptrons[addr].resize(BP_PERCEPTRON_HIST_LENGTH + 1, 0);
+      }
+      return perceptrons[addr];
+    }
+  }
+
+  // void reset_perceptron() {
+  //   std::fill(perceptron.begin(), perceptron.end(), 0);
+  // }
+
   void set_tag(Addr addr) {
-    tag = addr;
+    if (BP_PERCEPTRON_NO_ALIASING == FALSE) {
+      tag = addr;
+    }
   }
 };
 
@@ -61,7 +98,7 @@ struct Perceptron_State {
   // std::vector<std::vector<int> > ppt;
   std::vector<Entry> ppt;
   // TODO: if no branch aliasing
-  std::map<Addr, std::vector<int> > full_ppt;
+  // std::map<Addr, std::vector<int> > full_ppt;
 };
 
 std::vector<Perceptron_State> perceptron_state_all_cores;
@@ -113,11 +150,13 @@ void bp_perceptron_init() {
     // TODO: the macro table size
     perceptron_state.ppt.resize(BP_PERCEPTRON_TABLE_SIZE);
     // for(auto& perceptron : perceptron_state.ppt) {
-    for(auto& entry : perceptron_state.ppt) {
-      // TODO: parameterize
-      // add 1 for the branch itself
-      entry.perceptron.resize(BP_PERCEPTRON_HIST_LENGTH + 1, 0);
-      entry.tag = 0;
+    if (BP_PERCEPTRON_NO_ALIASING == FALSE) {
+      for(auto& entry : perceptron_state.ppt) {
+        // TODO: parameterize
+        // add 1 for the branch itself
+        entry.perceptron.resize(BP_PERCEPTRON_HIST_LENGTH + 1, 0);
+        entry.tag = 0;
+      }
     }
   }
 }
@@ -133,12 +172,20 @@ uns8 bp_perceptron_pred(Op* op) {
   // const std::vector<int>& perceptron = perceptron_state.ppt[ppt_index];
   // non-const
   Entry& entry = perceptron_state.ppt[ppt_index];
-  const std::vector<int>& perceptron = entry.perceptron;
+  // get tag before get perceptron
+  Addr tag = entry.get_tag(addr);
+  const std::vector<int>& perceptron = entry.get_perceptron(addr);
 
-  // if (addr != entry.tag) {
-  //   entry.set_tag(addr);
-  //   entry.reset_perceptron();
-  // }
+  // assume 0 is the initial value
+  if (tag == 0) {
+    STAT_EVENT(proc_id, BP_PERCEPTRON_TABLE_COMPULSARY);
+    entry.set_tag(addr);
+  } else if (addr != tag) {
+    STAT_EVENT(proc_id, BP_PERCEPTRON_TABLE_CONFLICT);
+    entry.set_tag(addr);
+  } else {
+    STAT_EVENT(proc_id, BP_PERCEPTRON_TABLE_HIT);
+  }
 
   // y
   const int weighted_sum = dot_product(hist, perceptron);
@@ -166,15 +213,15 @@ void bp_perceptron_update(Op* op) {
   const uns32 ppt_index    = get_ppt_index(addr);
   // std::vector<int>& perceptron = perceptron_state.ppt[ppt_index];
   Entry& entry = perceptron_state.ppt[ppt_index];
-  std::vector<int>& perceptron = entry.perceptron;
+  std::vector<int>& perceptron = entry.get_perceptron(addr);
 
   // TODO: has the history already shifted in?
   const int weighted_sum   = dot_product(hist, perceptron);
   // y_out
   int training_switch = 0;
-  if(weighted_sum > (int)BP_PERCEPTRON_THRESHOLD) {
+  if(weighted_sum > BP_PERCEPTRON_THRESHOLD) {
     training_switch = 1;
-  }else if(weighted_sum < -1 * (int)BP_PERCEPTRON_THRESHOLD) {
+  }else if(weighted_sum < -1 * BP_PERCEPTRON_THRESHOLD) {
     training_switch = -1;
   }
 
